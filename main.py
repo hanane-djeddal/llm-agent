cefd568a7bdf20d22c07a96686cff299846fcd02
ERBACHER Pierre <pierre.erbacher@gmail.com>
2024-04-29 09:41:53 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from agent import Agent
+from tool import SearchTool
+
+if __name__=='_main_':
+
+    question = 'what is the capital of ..'
+    tools = [SearchTool(name = 'search', index='robust04', start_token='boq', end_token='eoq')]
+    agent = Agent( model = 'mymodel', tools = tools)
+    kwargs = {'do_sample': True, 'top_p': 0.9}
+    answer = agent.generate(question , **kwargs)
